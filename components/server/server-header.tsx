a8843c25131238214df1e617ea88e2d457f5ecd9
deepak goyal <deepakgoyal1886@gmail.com>
2024-03-24 01:19:37 +0530

    feat: added manage member functionality

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -61,7 +61,10 @@ dark:hover:bg-zinc-700/50 transition "
           </DropdownMenuItem>
         )}
         {isAdmin && (
-          <DropdownMenuItem className="  px-3 py-2 text-sm cursor-pointer">
+          <DropdownMenuItem
+            onClick={() => onOpen("members", { server: server })}
+            className="  px-3 py-2 text-sm cursor-pointer"
+          >
             Manage Members
             <User className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
