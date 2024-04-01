8157ed13066af5426f9f8344d23b9bc291413b85
deepak goyal <deepakgoyal1886@gmail.com>
2024-04-02 00:37:10 +0530

    feat: added message edit functionality

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -1,11 +1,19 @@
 "use client";
 
 import { Member, Message, Profile } from "@prisma/client";
+import { format } from "date-fns";
 import ChatWelcome from "./chat-welcome";
 import { useChatQuery } from "@/hooks/use-chat-query";
 import { Loader2, ServerCrash } from "lucide-react";
 import { Fragment } from "react";
+import ChatItem from "./chat-item";
 
+const DATE_FORMAT = "d MMM yyyy, HH:mm";
+
+type MessageWithMemberWithProfile = Message & {
+  member: Member;
+  profile: Profile;
+};
 interface ChatMessagesProps {
   name: string;
   member: Member;
@@ -17,11 +25,6 @@ interface ChatMessagesProps {
   paramValue: string;
   type: "channel" | "conversation";
 }
-
-type MessageWithMemberWithProfile = Message & {
-  member: Member;
-  profile: Profile;
-};
 
 const ChatMessages = ({
   name,
@@ -78,7 +81,20 @@ const ChatMessages = ({
         {data?.pages?.map((group, i) => (
           <Fragment key={i}>
             {group.items.map((message: MessageWithMemberWithProfile) => (
-              <div key={message.id}>{message.content}</div>
+              // <div key={message.id}>{message.content}</div>
+              <ChatItem
+                key={message.id}
+                id={message.id}
+                currentMember={member}
+                member={message.member}
+                content={message.content}
+                fileUrl={message.fileUrl}
+                deleted={message.deleted}
+                timeStamp={format(new Date(message.createdAt), DATE_FORMAT)}
+                isUpdated={message.updatedAt !== message.createdAt}
+                socketUrl={socketUrl}
+                socketQuery={socketQuery}
+              />
             ))}
           </Fragment>
         ))}
