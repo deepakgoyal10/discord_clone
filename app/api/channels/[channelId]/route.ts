eee40c89856bc994a03cccf7a35c2d75be245fd5
deepak goyal <deepakgoyal1886@gmail.com>
2024-04-07 14:33:23 +0530

    fix: fixed dempoyment errors

--- app/api/channels/[channelId]/route.ts
+++ app/api/channels/[channelId]/route.ts
@@ -54,24 +54,23 @@ export async function PATCH(
   req: Request,
   { params }: { params: { channelId: string } }
 ) {
+  const profile = await currentProfile();
+  const { searchParams } = new URL(req.url);
+  const { name, type } = await req.json();
+  const serverId = searchParams.get("serverId");
+  if (!profile) {
+    return new NextResponse("Unauthorized", { status: 401 });
+  }
+  if (!serverId) {
+    return new NextResponse("Server ID missing", { status: 400 });
+  }
+  if (!params.channelId) {
+    return new NextResponse("Channel ID missing", { status: 400 });
+  }
+  if (name == "general") {
+    return new NextResponse("Name cannot be 'general'", { status: 400 });
+  }
   try {
-    const profile = await currentProfile();
-    const { searchParams } = new URL(req.url);
-    const { name, type } = await req.json();
-    const serverId = searchParams.get("serverId");
-    if (!profile) {
-      return new NextResponse("Unauthorized", { status: 401 });
-    }
-    if (!serverId) {
-      return new NextResponse("Server ID missing", { status: 400 });
-    }
-    if (!params.channelId) {
-      return new NextResponse("Channel ID missing", { status: 400 });
-    }
-    if (name == "general") {
-      return new NextResponse("Name cannot be 'general'", { status: 400 });
-    }
-
     const server = await db.server.update({
       where: {
         id: serverId,
